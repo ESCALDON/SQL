69c7c138c5da125df1e9675630e9aaafbf41a431
Sergio Leal Priere <sergioleal1313@gmail.com>
2023-01-19 14:03:56 +0100

    Ejercicio 17 SQL

--- SegundoGrupoConsultasSQL.sql
+++ SegundoGrupoConsultasSQL.sql
@@ -3,3 +3,9 @@ select distinct Genre,MAX(Metacritic) as "Puntuacion Maxima", MIN(Metacritic) as
 from juegos
 group by Genre
 order by MAX(Metacritic) desc, MIN(Metacritic);
+
+-- 17 Usando LIMIT, devuelve el top 10 de juegos con mayor puntuación del 2012.
+select distinct top 10 Genre,MAX(Metacritic) 
+from juegos
+where ReleaseDate = '2012'
+group by Genre;
