3342170b783d1a663206f977657ff7c2a74d1f3c
Abraham Jesús Lorenzo Guerra <abrahamlorenzo2912@gmail.com>
2023-01-19 13:28:57 +0000

    Tercera consulta
    Consulta nº3

--- PrimerGrupoDeConsultasSQL.sql
+++ PrimerGrupoDeConsultasSQL.sql
@@ -8,3 +8,8 @@ from juegos;
 --2. Lista todos los diferentes géneros, eliminando duplicados. Se usa distinct para eliminar los duplicados en sql
 Select distinct genre
 from juegos;
+
+--3. Lista únicamente aquellos juegos que puedan ser jugados únicamente en Linux
+select Nombre
+from juegos
+where PlatformLinux='true' and PlatformMac='false' and PlatformWindows='false'
