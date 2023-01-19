313db59b13467bedaa7547052990bf91e471552a
Abraham Jesús Lorenzo Guerra <abrahamlorenzo2912@gmail.com>
2023-01-19 13:26:56 +0000

    Segunda consulta
    Consulta nº2

--- PrimerGrupoDeConsultasSQL.sql
+++ PrimerGrupoDeConsultasSQL.sql
@@ -4,3 +4,7 @@ go
 --1. Lista todas las columnas de la tabla.
 select *
 from juegos;
+
+--2. Lista todos los diferentes géneros, eliminando duplicados. Se usa distinct para eliminar los duplicados en sql
+Select distinct genre
+from juegos;
