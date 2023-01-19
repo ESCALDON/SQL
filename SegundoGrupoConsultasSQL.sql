19fa30e8487a865e58118521addf60e3bcaedaa2
Sergio Leal Priere <sergioleal1313@gmail.com>
2023-01-19 13:43:18 +0100

    Ejercicio 16 SQL

---
+++ SegundoGrupoConsultasSQL.sql
@@ -0,0 +1,5 @@
+-- 16 ¿Cuál es la máxima, mínima y puntuación mínima por género?
+select distinct Genre,MAX(Metacritic) as "Puntuacion Maxima", MIN(Metacritic) as "Puntuacion Minima" 
+from juegos
+group by Genre
+order by MAX(Metacritic) desc, MIN(Metacritic);
