10c94893499036fb586505d95dfab8f0292fc4e0
Abraham Jesús Lorenzo Guerra <abrahamlorenzo2912@gmail.com>
2023-01-22 20:58:11 +0000

    Consulta 9
    Consulta nº9 terminada y consulta nº7 corregida. Consultas transferidas a otro fichero llamado ConsultasSQL.sql

--- ConsultasSQL.sql
+++ ConsultasSQL.sql
@@ -24,3 +24,22 @@ order by Metacritic desc
 select nombre
 from juegos
 where SupportedLanguages not like '%French%' and SupportedLanguages like '%Spanish%'
+
+--6. Lista el nombre del juego, su precio inicial, su precio final y la diferencia entre ambos en una nueva columna.
+select Nombre, PriceInitial, PriceFinal, PriceInitial-PriceFinal
+from juegos
+
+--7. ¿Cuántos juegos hay del siglo pasado?
+select count(*) as NumeroDeJuegos
+from juegos
+where ReleaseDate between '1900' and '1999'
+
+--8. Devuelve todos los juegos que terminen o en N o en O.
+select nombre
+from juegos
+where nombre like '%N' or nombre like '%O'
+
+--9. Devuelve todos los juegos que empiecen por A y que se puedan jugar con mando.
+select nombre
+from juegos
+where ControllerSupport='true' and nombre like 'A%'
