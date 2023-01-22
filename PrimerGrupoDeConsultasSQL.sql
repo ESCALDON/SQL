6abb1443bfbdd519bb41c6a759967d91c4e39135
Abraham Jesús Lorenzo Guerra <abrahamlorenzo2912@gmail.com>
2023-01-22 20:49:37 +0000

    Consulta 7
    Consulta nº7

--- PrimerGrupoDeConsultasSQL.sql
+++ PrimerGrupoDeConsultasSQL.sql
@@ -23,5 +23,14 @@ order by Metacritic desc
 --5. Lista todos aquellos juegos que puedan ser jugados en Español pero no en Frances
 select nombre
 from juegos
-where SupportedLanguages !='french' 
+where SupportedLanguages not like '%french%' and SupportedLanguages like '%spanish%'
 
+--6 Lista el nombre del juego, su precio inicial, su precio final y la diferencia entre ambos en una nueva columna.
+select Nombre, PriceInitial, PriceFinal, PriceInitial-PriceFinal
+from juegos
+
+--7¿Cuántos juegos hay del siglo pasado?
+--set dateformat dmy
+select nombre
+from juegos
+where ReleaseDate between '1900' and '1999'
